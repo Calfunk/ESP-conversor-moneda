b8246933584b09eb91b5a4d9651f4ab4611b1851
Calfunk <calfunaoe2@gmail.com>
2023-04-29 21:21:21 -0400

    precio del mercado: 29/05/2023

--- src/conversorMonedas/function.java
+++ src/conversorMonedas/function.java
@@ -9,7 +9,7 @@ public class function {
 	
     public void ConvertirMonedas(double Minput) {
     	String opcion = (JOptionPane.showInputDialog(null, 
-    			"Elije la moneda a la que deseas convertir tu dinero ", "Monedas", 
+    			"Elije la moneda a la que deseas convertir tu dinero (precio del mercado: 29/05/2023) ", "Monedas", 
     			JOptionPane.PLAIN_MESSAGE, null, new Object[] 
     			{"De Pesos a Dólar", "De Pesos a Euro", "De Pesos a Libras","De Pesos a Yen","De Pesos a Won Coreano","De Dólar a Pesos", "De Euro a Pesos", "De Libras a Pesos","De Yen a Pesos","De Won Coreano a Pesos"}, 
     			"Seleccion")).toString();
